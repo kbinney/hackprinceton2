79156f2c7cab62592f577643ad8e7b9ad764687b
kbinney <kbinney@college.harvard.edu>
2017-04-01 02:02:18 -0400

    template

--- app.py
+++ app.py
@@ -81,6 +81,57 @@ def send_message(recipient_id, message_text):
         log(r.status_code)
         log(r.text)
 
+def sendGenericMessage(recipientId):
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    messageData = json.dumps({
+    "recipient": {
+      "id": recipientId
+    },
+    "message": {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "rift",
+            "subtitle": "Next-generation virtual reality",
+            "item_url": "https://www.oculus.com/en-us/rift/",
+            "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+            "buttons": [{
+              "type": "web_url",
+              "url": "https://www.oculus.com/en-us/rift/",
+              "title": "Open Web URL"
+            }, {
+              "type": "postback",
+              "title": "Call Postback",
+              "payload": "Payload for first bubble",
+            }],
+          }, {
+            "title": "touch",
+            "subtitle": "Your Hands, Now in VR",
+            "item_url": "https://www.oculus.com/en-us/touch/",
+            "image_url": "http://messengerdemo.parseapp.com/img/touch.png",
+            "buttons": [{
+              "type": "web_url",
+              "url": "https://www.oculus.com/en-us/touch/",
+              "title": "Open Web URL"
+            }, {
+              "type": "postback",
+              "title": "Call Postback",
+              "payload": "Payload for second bubble",
+            }]
+          }]
+        }
+      }
+    }
+  })
+  r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+  callSendAPI(messageData);
 
 def log(message):  # simple wrapper for logging to stdout on heroku
     print str(message)
