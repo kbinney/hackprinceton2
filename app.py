b74c8cbaba9508aac5f901027f98505dc52d4906
kbinney <kbinney@college.harvard.edu>
2017-04-01 01:55:07 -0400

    working on template

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+# starter code from tutorial on FB chat bots by Hartley Brody.
+# https://blog.hartleybrody.com/fb-messenger-bot/
+
 import os
 import sys
 import json
@@ -38,7 +41,8 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-
+                    if keyword(message_text):
+                        send_message(sender_id, "special")
                     send_message(sender_id, "new again!")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
@@ -51,7 +55,8 @@ def webhook():
                     pass
 
     return "ok", 200
-
+def key_word(message):
+    return message == "home"
 
 def send_message(recipient_id, message_text):
 
