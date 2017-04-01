7da44d4fe0a6223d4c11dac4a12f584b68861cd8
kbinney <kbinney@college.harvard.edu>
2017-04-01 02:08:17 -0400

    keyword checking

--- app.py
+++ app.py
@@ -41,9 +41,10 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    #if keyword(message_text):
-                        #send_message(sender_id, "special")
-                    send_message(sender_id, "new again!")
+                    if keyword(message_text):
+                        send_message(sender_id, "special")
+                    else:
+                        send_message(sender_id, "new again!")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
@@ -57,7 +58,7 @@ def webhook():
     return "ok", 200
 
 def key_word(message):
-    return message == "home"
+    return (message == "home")
 
 def send_message(recipient_id, message_text):
 
