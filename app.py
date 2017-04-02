177e650aad940a1a3c2f8c33b87a899deaca8c49
kbinney <kbinney@college.harvard.edu>
2017-04-01 21:48:08 -0400

    logging messages

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def webhook():
 
                 if messaging_event.get("message"):  # someone sent us a message
                     log("message")
-                    if messaging_event["message"]["is_echo"]:
+                    if "is_echo" in messaging_event["message"]:
                         return ("ok", 200)
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
