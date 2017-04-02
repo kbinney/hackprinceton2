c412190f8bbc7e0f864b2ee4fd67e5e5cc7b3a65
kbinney <kbinney@college.harvard.edu>
2017-04-01 21:43:54 -0400

    syntax

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def webhook():
             for messaging_event in entry["messaging"]:
 
                 if messaging_event.get("message"):  # someone sent us a message
-                    if messaging_event["message"["is_echo"]]:
+                    if messaging_event["message"]["is_echo"]:
                         return ("ok", 200)
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
